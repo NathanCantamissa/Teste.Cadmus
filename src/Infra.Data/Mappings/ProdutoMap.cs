6994abe2126fb1af6ebe2db8093d3f1a7155065d
Nathan de Souza Cantamissa <nathan.nsc2@gmail.com>
2021-08-24 23:37:48 -0300

    Mapeamentos criados

---
+++ src/Infra.Data/Mappings/ProdutoMap.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infra.Data.Mappings
+{
+    public class ProdutoMap : IEntityTypeConfiguration<Produto>
+    {
+        public void Configure(EntityTypeBuilder<Produto> builder)
+        {
+            builder.ToTable("Produtos");
+
+            builder.HasKey(x => x.Id);
+
+            builder.Property(x => x.Descricao).HasColumnType("varchar(100)").IsRequired();
+            builder.Property(x => x.Valor).HasColumnType("decimal(100,2)").IsRequired();
+            builder.Property(x => x.Foto).HasColumnType("varchar(100)").IsRequired();
+        }
+    }
+}
