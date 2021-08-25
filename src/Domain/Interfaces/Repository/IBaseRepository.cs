f02385106c9eb0e5e163b96254a1246576152538
Nathan de Souza Cantamissa <nathan.nsc2@gmail.com>
2021-08-25 19:40:23 -0300

    Criando repositórios

---
+++ src/Domain/Interfaces/Repository/IBaseRepository.cs
@@ -0,0 +1,25 @@
+﻿using Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace Domain.Interfaces.Repository
+{
+    public interface IBaseRepository<TEntity> : IDisposable where TEntity : Entity
+    {
+        Task Adicionar(TEntity entity);
+
+        Task<TEntity> ObterPorId(Guid id);
+
+        Task<List<TEntity>> ObterTodos();
+
+        Task Atualizar(TEntity entity);
+
+        Task Remover(Guid id);
+
+        Task<IEnumerable<TEntity>> Buscar(Expression<Func<TEntity, bool>> predicate);
+
+        Task<int> SaveChanges();
+    }
+}
---
+++ src/Infra.Data/Repositories/ProdutoRepository.cs
@@ -0,0 +1,13 @@
+﻿using Domain.Interfaces.Repository;
+using Domain.Models;
+using Infra.Data.Context;
+
+namespace Infra.Data.Repositories
+{
+    public class ProdutoRepository : BaseRepository<Produto>, IProdutoRepository
+    {
+        public ProdutoRepository(ContextoEntity db) : base(db)
+        {
+        }
+    }
+}
