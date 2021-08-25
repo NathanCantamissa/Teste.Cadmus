c3565682ac17ba2ad6a09e2f2ec8e8f8d8e4eda4
Nathan de Souza Cantamissa <nathan.nsc2@gmail.com>
2021-08-25 19:24:19 -0300

    Configuração do identity

---
+++ src/Api/Configuration/IdentityConfig.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Infra.Data.Context;
+using Microsoft.AspNetCore.Identity;
+
+namespace Api.Configuration
+{
+    public static class IdentityConfig
+    {
+        public static IServiceCollection AddIdentityConfig(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ContextoIdentity>(options
+                   => options.UseSqlServer(configuration.GetConnectionString("DefaultConnection")));
+
+            services.AddIdentity<IdentityUser, IdentityRole>()
+                .AddEntityFrameworkStores<ContextoIdentity>()
+                .AddDefaultTokenProviders();
+
+            return services;
+        }
+    }
+}
